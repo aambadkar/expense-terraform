91d568153d4f40dad948a4eccb86b9aab9d40809
aambadkar <khodaskar.ankita@gmail.com>
2023-10-10 20:56:43 +0300

    expense script

--- modules/app/userdata.sh
+++ modules/app/userdata.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 
-dnf install python3.11-pip -y
+dnf install python3.11-pip ansible -y | tee -a /opt/userdata.log
 pip3.11 install boto3 botocore
-ansible-pull -i localhost, -U https://github.com/aambadkar/infra-ansible main.yml -e role_name=${role_name}
+ansible-pull -i localhost, -U https://github.com/aambadkar/infra-ansible main.yml -e role_name=${role_name} | tee -a /opt/userdata.log
